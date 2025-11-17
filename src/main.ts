42060e79559dd8fef92075c5ff1a5d06934e23f3
Marcelo Junior <Marcelo.Junior@fit-tecnologia.org.br>
2025-11-17 09:24:11 -0400

    Vercel added on main.ts

--- src/main.ts
+++ src/main.ts
@@ -10,7 +10,7 @@ async function bootstrap() {
   app.enableCors({
     origin: [
       'http://localhost:4200',
-      'https://task-master-frontend-seven.vercel.app/'
+      'https://task-master-frontend-seven.vercel.app/',
     ],
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     credentials: true,
@@ -24,14 +24,21 @@ async function bootstrap() {
     .addTag('auth')
     .addBearerAuth()
     .build();
-
+  
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
   const port = configService.get<number>('PORT') || 3000;
-  await app.listen(port);
+  await app.listen(process.env.PORT || port);
 
-  console.log(`Backend rodando em http://locahost:${port}`);
-  console.log(`Swagger disponível em http://localhost:${port}/api`);
+  // console.log(`Backend rodando em http://locahost:${port}`);
+  console.log(`Backend rodando na porta ${process.env.PORT || port}`);
+
+  if (process.env.NODE_ENV !== 'production') {
+    console.log(`Swagger disponível em http://localhost:${process.env.PORT || port}/api`);
+  }
+
+  // console.log(`Swagger disponível em http://localhost:${port}/api`);
 }
+
 bootstrap();
